fa640a6527a97dc935c008054a3353d8d764a2cc
linguopeng <linguopeng@cryptape.com>
2022-10-13 18:13:24 +0800

    test: fix prepare sh

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,8 @@ git clone https://github.com/gpBlockchain/startBlockchain.git
 cd startBlockchain/ckbLightClient
 sh prepare.sh
 sh start.sh
+sh status.sh
+cat ckb-light-client/target/release/node.log
 echo "prepare ckbLightClient finish"
 cd ../../
 git clone https://github.com/TheWaWaR/ckb-cli-light-client.git

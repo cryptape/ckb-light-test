a673e9e67a82182f9b69e12216a8c6a664b1e3e1
gpBlockchain <744158715@qq.com>
2025-06-13 17:25:32 +0800

    update ckb-light-client version

--- prepare.sh
+++ prepare.sh
@@ -2,7 +2,6 @@ mkdir tmp
 cd tmp
 git clone https://github.com/gpBlockchain/startBlockchain.git
 cd startBlockchain
-git checkout gp/update-fee-rate-checker
 cd ckbLightClient
 sh prepare.sh
 sh start.sh
